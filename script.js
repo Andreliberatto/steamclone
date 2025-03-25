a4f97ec1cb27e04acd1ec0ff5b55a3d106c701d0
Andé <115594252+Andreliberatto@users.noreply.github.com>
2025-03-25 11:46:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.login').addEventListener('click', () => {
+    alert('Funcionalidade de login ainda não implementada.');
+});
+
+document.querySelector('.signup').addEventListener('click', () => {
+    alert('Funcionalidade de cadastro ainda não implementada.');
+});
